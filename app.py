1bd73414d2749e6736ec8fac4cb6185c131bf76f
David Ames Jr <readytowin2298@gmail.com>
2022-02-12 17:31:17 -0600

    got point in polygon running, TBD: convert gps coords to floating point decimal, setup up command-line access to function

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import alphashape
-import matplotlib.pyplot as plt
-from descartes import PolygonPatch
+from shapely.geometry import Point, Polygon
 
-# Define input points
-points = [(0., 0.), (0., 1.), (1., 1.), (1., 0.), (0.5, 0.5)]
+# Create Point objects, customer home coordinates
+p1 = Point(24.952242, 60.1696017)
+p2 = Point(24.976567, 60.1612500)
 
-# Define alpha parameter
-alpha = 0.
+# Create a Polygon
+coords = [(24.950899, 60.169158), (24.953492, 60.169158), (24.953510, 60.170104), (24.950958, 60.169990)]
+poly = Polygon(coords)
 
-# Generate the alpha shape
-alpha_shape = alphashape.alphashape(points, alpha)
+print(p1)
+print(p2)
+print(coords)
+print(poly)
 
-# Initialize plot
-fig, ax = plt.subplots()
+print("Is p1 inside the polygon? ", p1.within(poly))
+print("Is p2 inside polygon? ", p2.within(poly))
 
-# Plot input points
-ax.scatter(*zip(*points))
 
-# Plot alpha shape
-ax.add_patch(PolygonPatch(alpha_shape, alpha=.2))
 
-plt.show()
+def ftth_checker(neighborhood, home):
+    print(neighborhood)
