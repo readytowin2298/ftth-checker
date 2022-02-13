54deac746d77ab4bc86efe03a0599d0eeb9cd69d
Mr. Ames <readytowin2298@gmail.com>
2022-02-12 21:39:32 -0600

    think it works, need to get test data from GEarth

---
+++ testCoords.py
@@ -0,0 +1,11 @@
+testCoords = [{
+		"lattitude": """32°26'37.31"N""",
+		"longitude": """97° 4'53.41"W"""
+	}, {
+		"lattitude": """32°27'7.61"N""",
+		"longitude": """97° 5'7.56"W"""
+	},{
+		"lattitude": """32°26'54.25"N""",
+		"longitude": """97° 5'45.96"W"""
+	}]
+
