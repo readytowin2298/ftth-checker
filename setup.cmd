3d791547400e80041445bced8bbbc6cff49c9001
Mr. Ames <readytowin2298@gmail.com>
2022-02-13 01:23:18 -0600

    edited setup script to make it hopfeully executable on both linux or windows, tested on Ubuntu

--- setup.cmd
+++ setup.cmd
@@ -1,3 +1,26 @@
+: # This is a special script which intermixes both sh
+: # and cmd code. It is written this way because it is
+: # used in system() shell-outs directly in otherwise
+: # portable code. See https://stackoverflow.com/questions/17510688
+: # for details.
+
+:; echo Using Linux? I like you
+:; [ ! -d "/venv" ] && python3 -m venv venv
+:; alias activate=". ./venv/bin/activate"
+:; activate
+:; pip install -r requirements.txt
+:; echo ==============================================
+:; echo HHII TIM!
+:; echo please exit the virtual environment before closing terminal
+:; echo enter command "deactivate"
+:; echo failing to do so can cause issues during later use, if in doubt delete /venv and /__pycache__
+:; echo ==============================================
+:; python3 test_app.py
+:; deactivate
+:; exit
+
+
+
 @ECHO OFF
 echo hi Tim!
 echo %~dp0
@@ -5,23 +28,25 @@ IF NOT EXIST "%~dp0\venv\scripts\activate.bat" (
     python -m venv venv
     venv\scripts\activate.bat
     pip install -r requirements.txt
-    python test_app.py
+    
     echo ==============================================
     echo HHII TIM!
     echo please exit the virtual environment before closing terminal
     echo enter command "deactivate"
     echo failing to do so can cause issues during later use, if in doubt delete /venv and /__pycache__
     echo ==============================================
+    python test_app.py
     cmd /k
 ) ELSE (
     venv\scripts\activate.bat
     pip install -r requirements.txt
-    python test_app.py
+    
     echo ==============================================
     echo HHII TIM!
     echo please exit the virtual environment before closing terminal
     echo enter command "deactivate"
     echo failing to do so can cause issues during later use, if in doubt delete /venv and /__pycache__
     echo ==============================================
+    python test_app.py
     cmd /k
 )
